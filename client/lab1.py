190c1bcbceb182bf7d8c7e111fd44e1b6b072d8a
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-09-26 16:07:10 -0700

    Established connection with local gcdserv

--- client/lab1.py
+++ client/lab1.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+""""
+CPSC 5520, Seattle University
+This is free and unencumbered software released into the public domain.
+:Authors: Narissa Tsuboi
+:Version: 1
+"""
+import socket
+import sys
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("Client Main")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if len(sys.argv) != 3:
+        print("Usage: python lab1.py HOST PORT")
+        exit(1);
+
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        s.sendall(b'JOIN')
+        data = s.recv(1024)
+    print('Received', repr(data))
