bf236026925e0adc204b07644fd7325233dc1f19
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-10-01 12:33:06 -0700

    Added lab2.py

---
+++ 2-bully/lab2.py/lab2.py
@@ -0,0 +1,13 @@
+""""
+CPSC 5520, Seattle University
+This is free and unencumbered software released into the public domain.
+:Authors: Narissa Tsuboi
+:Version: 1
+:brief:
+"""
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('Bully')
+
